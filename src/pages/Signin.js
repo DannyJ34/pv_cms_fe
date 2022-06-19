9e48c775542f85818dbeae452231355676c4abb2
DannyJ34 <tisalron@naver.com>
2022-06-19 19:52:15 +0900

    signin 공부 - style component, justify, mui checkbox

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,48 +2,84 @@ import React, { useEffect, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
 
 const Signin = () => {
 
     const [value, setValue] = useState("");
+    const [signInMaintainStatus, setSignInMaintainStatus] = useState(false);
 
     const handleChange = (event) => {
         setValue(event.target.value);
     };
 
+    const handleSignInMaintainStatusChange = (event) => {
+        setSignInMaintainStatus(event.target.checked);
+    };
+
+    // const underlineLink = styled(Link)
+    //     color: blue;
+
+
     useEffect(() => {
         console.log("value: " + value)
     }, [value])
 
+    useEffect(() => {
+        console.log("signInMaintainStatus: " + signInMaintainStatus)
+    }, [signInMaintainStatus])
+
+    useEffect(() => {
+        // get signInMaintainStatus
+    }, [])
+
     return (
         <>
-            <div>
-                이건 로그인 페이지임
-            </div>
             <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image="https://user-images.githubusercontent.com/44333767/162618795-4cf597b4-0337-4063-aada-f45598e39608.png"
-                    alt="green iguana"
-                />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
+                    <Typography gutterBottom variant="h6" component="div">
+                        Sign in
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
+                    <Typography variant="body4" color="text.secondary">
+                        PortValue에 오신걸 환영합니다🎉
                     </Typography>
                 </CardContent>
+                <TextField id="outlined-basic" label="이메일" variant="outlined" />
+                <TextField id="outlined-basic" label="비밀번호" variant="outlined" />
+                <Button variant="contained" disabled>
+                    로그인
+                </Button>
+                <FormGroup>
+                    <FormControlLabel onChange={handleSignInMaintainStatusChange} checked={signInMaintainStatus} control={<Checkbox />} label="로그인 상태 유지" />
+                </FormGroup>
                 <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
+                    <Link href="https://member.daum.net/find/id.do?action=home" underline="always">
+                        {'아이디 찾기'}
+                    </Link>
+                    <Link href="https://member.daum.net/find/password.do?action=daumid-check" underline="always">
+                        {'비밀번호 찾기'}
+                    </Link>
+                    <Link href="https://member.nexon.com/join/join.aspx" underline="always">
+                        {'회원가입'}
+                    </Link>
+
+                    {/* style component  */}
+                    {/* <div>
+                            <body>
+                                <h1 style="color:blue; text-decoration: underline;">
+                                    아이디 찾기
+                                </h1>
+                            </body> 
+                    </div> */}
+
                 </CardActions>
             </Card>
             <Box
@@ -60,4 +96,7 @@ const Signin = () => {
     )
 };
 
-export default Signin; 
+export default Signin;
+
+
+
